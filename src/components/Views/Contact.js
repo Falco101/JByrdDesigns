bf9020d3e116f2d6450073eb81213ca88e43bdef
Jon Byrd <jonathan.byrd.2@gmail.com>
2017-03-06 10:55:55 -0500

    Contact Form

--- src/components/Views/Contact.js
+++ src/components/Views/Contact.js
@@ -4,16 +4,26 @@ class ContactComponent extends React.Component {
     constructor() {
         super();
     }
-    render () {
+    render() {
         return (
             <div>
-                <p>Contact</p>
+                <h1>Contact</h1>
+                <p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. Cras ultricies
+                    ligula sed magna dictum porta. Pellentesque in ipsum id orci porta dapibus.</p>
+                <form className="jbd-contact-form">
+                    <label>Email</label>
+                    <input type="email"></input>
+                    <label>Subject</label>
+                    <input type="text"></input>
+                    <label>Email Message</label>
+                    <textarea name="email-body" rows="10" cols="50"/>
+                    <input type="submit"/>
+                </form>
             </div>
         );
     }
 }
 
-ContactComponent.defaultProps = {
-};
+ContactComponent.defaultProps = {};
 
 export default ContactComponent;
