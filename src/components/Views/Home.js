52d53eaa6fdee5bf65992a0989509d56676db122
Jon Byrd <jonathan.byrd.2@gmail.com>
2017-05-12 18:15:06 -0400

    re-did logo and started work on portfolio

--- src/components/Views/Home.js
+++ src/components/Views/Home.js
@@ -3,9 +3,9 @@ import React from 'react';
 class HomeComponent extends React.Component {
   render() {
     return (
-      <div className='site-content'>
+      <div className='home-content'>
         <header>
-            <h1>JByrd Designs</h1>
+            <h1>J Byrd Designs</h1>
             <p>Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </header>
         <section className="design-offerings">
