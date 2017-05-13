d985a75b86f2cceec889272249fadb39d4af1de5
Jon Byrd <jonathan.byrd.2@gmail.com>
2017-05-13 02:47:49 -0400

    layout looks good now, needs content

--- src/components/Views/Home.js
+++ src/components/Views/Home.js
@@ -5,7 +5,7 @@ class HomeComponent extends React.Component {
     return (
       <div className='home-content'>
         <header>
-            <h1>J Byrd Designs</h1>
+            <h1>Jon Byrd Designs</h1>
             <p>Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </header>
         <section className="design-offerings">
--- src/components/Views/Portfolio.js
+++ src/components/Views/Portfolio.js
@@ -9,10 +9,23 @@ class PortfolioComponent extends React.Component {
             <div className='portfolio-content'>
                 <div className='portfolio-title'>
                     <h1>Design Portfolio</h1>
-                    <p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Pellentesque in ipsum id orci porta dapibus. Cras ultricies ligula sed magna dictum porta. Donec rutrum congue leo eget malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
+                    <p>Curabitur non nulla sit amet nisl tempus convallis quis
+                     ac lectus. Cras ultricies ligula sed magna dictum porta.
+                     Sed porttitor lectus nibh. Quisque velit nisi, pretium ut
+                      lacinia in, elementum id enim. Pellentesque in ipsum id
+                       orci porta dapibus. Cras ultricies ligula sed magna
+                        dictum porta. Donec rutrum congue leo eget malesuada.
+                         Praesent sapien massa, convallis a pellentesque nec,
+                          egestas non nisi. Lorem ipsum dolor sit amet,
+                           consectetur adipiscing elit. Curabitur non nulla
+                            sit amet nisl tempus convallis quis ac lectus.</p>
                 </div>
                 <div className='portfolio-graphics'>
                     <h2>Graphic Design</h2>
+                    <div className='portfolio-description-container'>
+                        <img src=''></img>
+                        <p></p>
+                    </div>
                 </div>
                 <div className='portfolio-web'>
                     <h2>Web Design</h2>
